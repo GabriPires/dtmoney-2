3d8102cdcd748a6b1785955b88b5dc0670340ffa
Gabriel Pires <gabpiresdev@gmail.com>
2022-08-29 19:03:03 -0300

    feat: transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,56 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles';
 
 export const Transactions = () => {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width={'50%'}>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 1.000,00</PriceHighlight>
+              </td>
+
+              <td>Venda</td>
+              <td>29/08/2022</td>
+            </tr>
+            <tr>
+              <td width={'50%'}>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 1.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>29/08/2022</td>
+            </tr>
+            <tr>
+              <td width={'50%'}>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 1.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>29/08/2022</td>
+            </tr>
+            <tr>
+              <td width={'50%'}>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 1.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>29/08/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 };

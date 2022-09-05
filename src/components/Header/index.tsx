f777ad03057cb343684c978636d8269b2a0f0e8c
Gabriel Pires <gabpiresdev@gmail.com>
2022-09-05 18:51:14 -0300

    feat: accessible new transactions modal with radix ui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,21 @@
+import * as Dialog from '@radix-ui/react-dialog';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
 
 import logoImg from '../../assets/logo.svg';
+import { NewTransactionModal } from '../NewTransactionModal';
 
 export const Header = () => {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.DialogTrigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.DialogTrigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
